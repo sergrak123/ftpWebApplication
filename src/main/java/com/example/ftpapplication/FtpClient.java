59d3895000dc6c0797b82b8baeb89f8255e93d4f
Sergey <sergey-707@mail.ru>
2022-04-26 16:33:33 +0400

    Adding apache net with maven/jar

--- src/main/java/com/example/ftpapplication/FtpClient.java
+++ src/main/java/com/example/ftpapplication/FtpClient.java
@@ -1,17 +1,34 @@
 package com.example.ftpapplication;
 
+//import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+
 import java.io.IOException;
+import java.util.logging.Logger;
 
 
 public class FtpClient {
+    private static Logger log = Logger.getLogger(FtpClient.class.getName());
+
 
     public FtpClient() throws IOException {
         FTPClient ftpClient = new FTPClient();
 
         try{
+            log.info("Some message11111111111");
             ftpClient.connect("91.222.128.11", 21);
             ftpClient.login("testftp_guest", "12345");
+            int replyCode = ftpClient.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+                log.info("Some message");
+            }
+
+
+//            ftpClient.logout();
         }
         catch (Exception exception){
 
@@ -20,6 +37,11 @@ public class FtpClient {
 //        ftpClient.connect(hostname, port);
 //        ftpClient.login(username, password);
 //        System.out.print(ftpClient.getReplyString());
+
+//        String server = "www.yourserver.net";
+//        int port = 21;
+//        String user = "username";
+//        String pass = "password";
     }
 
 
--- src/main/java/com/example/ftpapplication/MainView.java
+++ src/main/java/com/example/ftpapplication/MainView.java
@@ -1,9 +1,13 @@
 package com.example.ftpapplication;
 
 
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 
 import java.io.IOException;
 
@@ -12,8 +16,32 @@ import java.io.IOException;
 public class MainView extends VerticalLayout {
 
     public MainView () throws IOException {
-        add(new H2("Apps"));
-        FtpClient ftpClient = new FtpClient();
+        add(new H1("Ftp Web App"));
+//        FtpClient ftpClient = new FtpClient();
+
+        FTPClient ftpClient = new FTPClient();
+
+        try{
+            add(new H3("Starting server..."));
+
+            ftpClient.connect("91.222.128.11", 21);
+            ftpClient.login("testftp_guest", "12345");
+
+            int replyCode = ftpClient.getReplyCode();
+            add(new H3(String.valueOf(replyCode)));
+
+
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+                add(new H2("!!!"));
+            }
+
+
+//            ftpClient.logout();
+        }
+        catch (Exception exception){
+
+        }
+
 
     }
 }
