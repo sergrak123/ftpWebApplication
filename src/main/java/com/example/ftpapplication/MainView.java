f2e8c88eae6c0f5e008889184bbe5d879b473d0d
Sergey <sergey-707@mail.ru>
2022-04-26 18:00:14 +0400

    Adding file info

--- src/main/java/com/example/ftpapplication/MainView.java
+++ src/main/java/com/example/ftpapplication/MainView.java
@@ -5,9 +5,12 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
+import org.apache.commons.net.ftp.FTPFile;
 
 import java.io.IOException;
 
@@ -27,6 +30,7 @@ public class MainView extends VerticalLayout {
             ftpClient.connect("91.222.128.11", 21);
             ftpClient.login("testftp_guest", "12345");
 
+
             int replyCode = ftpClient.getReplyCode();
             add(new H3(String.valueOf(replyCode)));
 
@@ -34,6 +38,21 @@ public class MainView extends VerticalLayout {
             if (!FTPReply.isPositiveCompletion(replyCode)) {
                 add(new H2("!!!"));
             }
+
+            FTPFile[] ftpFiles = ftpClient.listFiles();
+
+            String info = "File number:" + ftpFiles.length + "\n"
+                    + ftpFiles[0].getName() + "\n"
+                    + ftpFiles[0].getGroup()+ "\n"
+                    + ftpFiles[0].getLink()+ "\n"
+                    + ftpFiles[0].getSize()+ "\n"
+                    + ftpFiles[0].getType()+ "\n"
+                    + ftpFiles[0].isDirectory()+ "\n"
+                    + ftpFiles[0].isFile()+ "\n";
+            TextArea tx = new TextArea();
+            tx.setValue(info);
+            add(tx);
+
 
 
 //            ftpClient.logout();
