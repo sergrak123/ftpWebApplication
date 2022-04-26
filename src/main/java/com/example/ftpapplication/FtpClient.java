badaa0beb31d980b6e4e758238d732762e94c138
Sergey <sergey-707@mail.ru>
2022-04-26 15:30:07 +0400

    Ref

--- src/main/java/com/example/ftpapplication/FtpClient.java
+++ src/main/java/com/example/ftpapplication/FtpClient.java
@@ -1,16 +1,22 @@
 package com.example.ftpapplication;
 
 import org.apache.commons.net.ftp.FTPClient;
-
 import java.io.IOException;
+
 
 public class FtpClient {
 
     public FtpClient() throws IOException {
         FTPClient ftpClient = new FTPClient();
 
-        ftpClient.connect("91.222.128.11", 21);
-        ftpClient.login("testftp_guest", "12345");
+        try{
+            ftpClient.connect("91.222.128.11", 21);
+            ftpClient.login("testftp_guest", "12345");
+        }
+        catch (Exception exception){
+
+        }
+
 //        ftpClient.connect(hostname, port);
 //        ftpClient.login(username, password);
 //        System.out.print(ftpClient.getReplyString());
--- src/main/java/com/example/ftpapplication/MainView.java
+++ src/main/java/com/example/ftpapplication/MainView.java
@@ -5,12 +5,15 @@ import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+import java.io.IOException;
+
 
 @Route
 public class MainView extends VerticalLayout {
 
-    public MainView () {
+    public MainView () throws IOException {
         add(new H2("Apps"));
+        FtpClient ftpClient = new FtpClient();
 
     }
 }
