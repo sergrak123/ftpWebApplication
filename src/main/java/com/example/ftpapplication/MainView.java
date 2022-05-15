883a318fac3cdf679b6dae8398f91c7ffd63f18f
Sergey <sergey-707@mail.ru>
2022-05-15 19:45:26 +0400

    Add interface

--- src/main/java/com/example/ftpapplication/MainView.java
+++ src/main/java/com/example/ftpapplication/MainView.java
@@ -1,39 +1,47 @@
 package com.example.ftpapplication;
 
 
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.details.DetailsVariant;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPReply;
-import org.apache.commons.net.ftp.FTPFile;
 
 import java.io.*;
 import java.net.Socket;
-import java.net.URL;
-import java.net.URLConnection;
 
 
 @Route
 public class MainView extends VerticalLayout {
 
-    Socket socket = null;
 
-    BufferedReader reader = null;
+    int totalSize = 0;
 
-    BufferedWriter writer = null;
+    public MainView() throws IOException {
 
-    boolean DEBUG = false;
-
-    public MainView () throws IOException {
+//        String host = "91.222.128.11";
+//        int port = 21;
+//        String user = "testftp_guest";
+//        String pass = "12345";
+//
+//        FTPData ftp = new FTPData(host, port, user, pass);
+//        ftp.getAllFiles("");
+//
+//        int totalSize = 0;
+//        for (String type : ftp.typeList.keySet()) {
+//
+//            int typeSize = ftp.typeList.get(type);
+//            String formatSize = FTPData.formatFileSize(typeSize);
+//            totalSize += typeSize;
+//            System.out.println(type + " " + formatSize);
+//        }
+//        System.out.println(FTPData.formatFileSize(totalSize));
 
 
         //frontend
@@ -45,17 +53,85 @@ public class MainView extends VerticalLayout {
         userField.setLabel("Username");
         passField.setLabel("Password");
 
+
         Button findButton = new Button("Начать");
         findButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        HorizontalLayout hl = new HorizontalLayout(hostField,userField,passField, findButton);
+        HorizontalLayout hl = new HorizontalLayout(hostField, userField, passField, findButton);
         hl.setAlignItems(Alignment.END);
 
-        VerticalLayout vr = new VerticalLayout(new H2("Ftp Client Web App"), hl);
+//        VerticalLayout vr = new VerticalLayout(new H2("Ftp Client Web App"), hl);
+//        vr.setAlignItems(Alignment.CENTER);
+//        add(vr);
+
+
+        //Вывод ссылок
+
+        UnorderedList content = new UnorderedList();
+        UnorderedList content2 = new UnorderedList();
+
+        Details details = new Details();
+        details.setOpened(true);
+        details.addThemeVariants(DetailsVariant.FILLED);
+        details.setSummaryText("Все файлы : ");
+
+        Details details2 = new Details();
+        details2.setOpened(true);
+        details2.addThemeVariants(DetailsVariant.FILLED);
+        details2.setSummaryText("Тип файла / размер файлов : ");
+
+        HorizontalLayout hl2 = new HorizontalLayout(details, details2);
+        hl2.setPadding(true);
+
+
+        VerticalLayout vr = new VerticalLayout(new H2("Ftp Client Web App"), hl, new H3(""), hl2);
         vr.setAlignItems(Alignment.CENTER);
         add(vr);
 
-        SimpleFTP simpleFTP = new SimpleFTP(hostField.getValue(),21, userField.getValue(), passField.getValue());
+        Scroller scroller = new Scroller(content);
+        scroller.setScrollDirection(Scroller.ScrollDirection.HORIZONTAL);
+
+        Scroller scroller2 = new Scroller(content2);
+        scroller2.setScrollDirection(Scroller.ScrollDirection.HORIZONTAL);
+        findButton.addClickShortcut(Key.ENTER);
+
+        findButton.addClickListener(clickEvent -> {
+            try {
+                SimpleFTP simpleFTP = new SimpleFTP(hostField.getValue(), 21, userField.getValue(), passField.getValue());
+                simpleFTP.getAllFiles("");
+                content.removeAll();
+                content2.removeAll();
+
+
+                for (String name : simpleFTP.fileList.keySet()) {
+
+                    int fileSize = simpleFTP.fileList.get(name);
+                    String formatFileSize = SimpleFTP.formatFileSize(fileSize);
+                    content.add(new ListItem(name + formatFileSize));
+                }
+                scroller.setContent(content);
+                details.setContent(scroller);
+                details.setSummaryText("Все файлы : " + simpleFTP.fileList.size());
+
+
+
+                for (String type : simpleFTP.typeList.keySet()) {
+
+                    int typeSize = simpleFTP.typeList.get(type);
+                    String formatTypeSize = SimpleFTP.formatFileSize(typeSize);
+                    totalSize += typeSize;
+                    System.out.println(type + " " + formatTypeSize);
+                    content2.add(new ListItem(type + formatTypeSize));
+                }
+                //
+
+                scroller2.setContent(content2);
+                details2.setContent(scroller2);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
 
     }
